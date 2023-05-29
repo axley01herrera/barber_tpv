e532f3471ea00415f53c022237f83f2d40a13f92
Axley Herrera <axley01herrera@gmail.com>
2023-05-29 20:36:58 +0100

    add db

---
+++ sql/barber_tpv.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 29-05-2023 a las 19:36:33
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `barber_tpv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `cost` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tpv`
+--
+
+DROP TABLE IF EXISTS `tpv`;
+CREATE TABLE IF NOT EXISTS `tpv` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_user` int(11) NOT NULL,
+  `fk_product` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `clave` varchar(999) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `last_name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `role` int(1) NOT NULL COMMENT '1 = Administrador 2 = Basico',
+  `status` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `clave`, `name`, `last_name`, `role`, `status`) VALUES
+(1, 'axley01herrera@gmail.com', '202cb962ac59075b964b07152d234b70', 'Axley', 'Herrera', 1, 1),
+(2, 'test@email.com', '123', 'Miguel', 'Alfonso', 1, 1),
+(3, 'carlos@email.com', NULL, 'Carlos', 'Osbaldo', 2, 1),
+(4, 'ariel@email.com', '202cb962ac59075b964b07152d234b70', 'Ariel', 'Duque', 2, 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
